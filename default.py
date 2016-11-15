c95736e4e218dbfcabd020c6d1214ecfad723ee8
Ryan Herbert <ryan.herbert@inria.fr>
2016-11-15 13:58:05 +0100

    default.py add profile list query to index

--- default.py
+++ default.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template
 
-from models import Report, Server, Field, Format
-from utils import get_mapped_dict, get_processor
-from comparator import Status, calculate_status
+from models import Profile, Stat
 
 api = Blueprint('api', __name__)
 
 @api.route('/')
 def index():
-    return render_template('index.html', servers=server_list, formats=formats)
+    profiles = Profile.query.all()
+    for profile in profiles:
+        profile.last_rank = 0
+    return render_template('index.html', profiles = profiles)
